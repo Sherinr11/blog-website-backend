9e8056abb2077a5cd043a48e5d66614de40c7589
Sherin Rose <sherinrose@anil.mac.mglabs>
2024-08-19 16:18:45 +0530

    commit cors

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express=require('express');
+const cors = require('cors');
 const app=express();
+
+app.use(cors());
 const blogRoutes=require('./routes/blogRoutes');
 const conn=require("./services/db");
 conn.dbConnection();
